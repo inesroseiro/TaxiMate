810347192be6d0a26e898eddf57836f60a31f814
inesroseiro <miroseiro@student.dei.uc.pt>
2017-12-16 18:04:46 +0000

    Ja ta carago

---
+++ TaxiMate/app/src/main/java/com/example/soren5/taximate/CreationActivity.java
@@ -0,0 +1,48 @@
+package com.example.soren5.taximate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class CreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = firebaseDatabase.getReference("message");
+
+        myRef.child("teste").setValue("Hello World!");
+
+        final Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                myRef.child("teste").addValueEventListener(new ValueEventListener() {
+                    public static final String TAG = "";
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        final TextView textView = (TextView) findViewById(R.id.textView);
+                        textView.setText(dataSnapshot.getValue(String.class));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "Failed to read value: ", databaseError.toException());
+                    }
+                });
+            }
+        });
+
+    }
+}
---
+++ TaxiMate/app/src/main/java/com/example/soren5/taximate/Pedido.java
@@ -0,0 +1,23 @@
+package com.example.soren5.taximate;
+
+/**
+ * Created by soren5 on 12/13/17.
+ */
+
+public class Pedido {
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNome2() {
+        return nome2;
+    }
+
+    public Pedido(String nome, String nome2) {
+        this.nome = nome;
+        this.nome2 = nome2;
+    }
+
+    private String nome;
+    private String nome2;
+}
